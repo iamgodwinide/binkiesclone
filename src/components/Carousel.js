aa67cb64fd90ce9b93e05c0aeb43a820d177e5a9
Godwin Idemudia <godwinide@gmail.com>
2022-10-11 20:07:49 +0100

    new

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -47,7 +47,7 @@ const CarouselCompnent = () => {
             itemClass="image-item"
             responsive={responsive}
         >
-            {images.slice.map(image => {
+            {images.map(image => {
                 return (
                     <Image
                         draggable={false}
